78aeb73e5b4ef3894f8d753510f3bafe0bb2fb59
Omarfadalla <omarfadalla@gmail.com>
2026-07-22 11:50:28 +0300

    added a dynamic crosshair

--- controllers/scripts/fps_controller.gd
+++ controllers/scripts/fps_controller.gd
@@ -1,6 +1,9 @@
 extends CharacterBody3D
 
+@export var SPEED_DEFAULT:float = 5.0
+@export var SPEED_CROUCH:float = 2.0
 @export var SPEED : float = 5.0
+@export var TOGGLE_CROUCH : bool = true
 @export var JUMP_VELOCITY : float = 4.5
 @export_range(5,10,0.1) var CROUCH_SPEED : float = 7.0
 @export var MOUSE_SENSITIVITY : float = 0.5
@@ -10,6 +13,7 @@ extends CharacterBody3D
 @export var ANIMATIONPLAYER : AnimationPlayer
 @export var CROUCH_SHAPECAST : Node3D
 
+var _speed : float
 var _mouse_input : bool = false
 var _rotation_input : float
 var _tilt_input : float
@@ -34,10 +38,14 @@ func _input(event):
 	
 	if event.is_action_pressed("exit"):
 		get_tree().quit()
-	if event.is_action_pressed("crouch"):
-		toggle_crouch()
-
-
+	if event.is_action_pressed("crouch") and _is_crouching == false and is_on_floor() and TOGGLE_CROUCH == false:
+		crouching(true) # hold to crouch 
+	if event.is_action_released("crouch") and TOGGLE_CROUCH == false: #releasing the crouch
+		if CROUCH_SHAPECAST.is_colliding() == false:
+			crouching(false)
+		if CROUCH_SHAPECAST.is_colliding() == true:
+			uncrouch_check()
+		
 func _update_camera(delta):
 	
 	# Rotates camera using euler rotation
@@ -61,6 +69,9 @@ func _ready():
 	# Get mouse input
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+	#setting the default speed of the player
+	_speed = SPEED_DEFAULT
+
 	# add crouch check shapecast collision
 	CROUCH_SHAPECAST.add_exception($".")
 
@@ -74,7 +85,7 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and _is_crouching == false:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -84,21 +95,43 @@ func _physics_process(delta):
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * _speed
+		velocity.z = direction.z * _speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, _speed)
+		velocity.z = move_toward(velocity.z, 0, _speed)
 
 	move_and_slide()
 
 func toggle_crouch():
 	if _is_crouching == true and CROUCH_SHAPECAST.is_colliding() == false:
-		ANIMATIONPLAYER.play("Crouch", -1 , -CROUCH_SPEED, true)
+		crouching(false)
 	elif _is_crouching == false:
-		ANIMATIONPLAYER.play("Crouch", -1, CROUCH_SPEED)
+		crouching(true)
+
+func uncrouch_check():
+	if CROUCH_SHAPECAST.is_colliding() == false:
+		crouching(false)
+	if CROUCH_SHAPECAST.is_colliding() == true:
+		await get_tree().create_timer(0.1).timeout
+		uncrouch_check()
+func crouching(state:bool):
+	match state:
+		true:
+			ANIMATIONPLAYER.play("Crouch", 0, CROUCH_SPEED)
+			set_movement_speed("crouching")
+		false:
+			ANIMATIONPLAYER.play("Crouch", 0, -CROUCH_SPEED, true)
+			set_movement_speed("default")
 
 func _on_animation_player_animation_started(anim_name: StringName) -> void:
 	if anim_name	 =="Crouch":
 		_is_crouching = !_is_crouching 
 	pass # Replace with function body.
+
+func set_movement_speed(state : String):
+	match state:
+		"default":
+			_speed = SPEED_DEFAULT
+		"crouching":
+			_speed = SPEED_CROUCH
