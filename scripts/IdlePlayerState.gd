1b7580f0d3c2a66048cab1ef1347b00a1d7d1a6f
Omarfadalla <omarfadalla@gmail.com>
2026-07-22 19:18:22 +0300

    added basic walking acceleration and deceleration

--- scripts/IdlePlayerState.gd
+++ scripts/IdlePlayerState.gd
@@ -1,9 +1,6 @@
 class_name IdlePlayerState
-
 extends State
 
-
-
 func update(delta):
-	if global.player.velocity.length() > 0:
+	if global.player.velocity.length() > 0 and global.player.is_on_floor():
 		transition.emit("WalkingPlayerState") 
