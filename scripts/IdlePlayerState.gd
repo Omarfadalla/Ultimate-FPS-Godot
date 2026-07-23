deaa1e2fb2cf50e1376051ac2fcb83d40d09f0b3
Omarfadalla <omarfadalla@gmail.com>
2026-07-23 16:33:17 +0300

    added a walking animation

--- scripts/IdlePlayerState.gd
+++ scripts/IdlePlayerState.gd
@@ -1,6 +1,12 @@
 class_name IdlePlayerState
 extends State
 
+@export var ANIMATION : AnimationPlayer
+
+
+func enter() -> void:
+	ANIMATION.pause()
+
 func update(delta):
 	if global.player.velocity.length() > 0 and global.player.is_on_floor():
 		transition.emit("WalkingPlayerState") 
--- scripts/WalkingPlayerState.gd
+++ scripts/WalkingPlayerState.gd
@@ -1,6 +1,17 @@
 class_name WalkingPlayerState
 extends State
 
+@export var ANIMATION : AnimationPlayer
+@export var TOP_ANIM_SPEED : float = 2.2
+
+func enter() -> void:
+	ANIMATION.play("Walking",-1.0,1.0)
+
 func update(delta: float) -> void:
+	set_animation_speed(global.player.velocity.length())
 	if global.player and global.player.velocity.length() == 0:
 		transition.emit("IdlePlayerState")
+
+func set_animation_speed(spd):
+	var alpha = remap(spd,0.0,global.player.SPEED_DEFAULT,0.0,1.0)
+	ANIMATION.speed_scale = lerp(0.0,TOP_ANIM_SPEED,alpha)
