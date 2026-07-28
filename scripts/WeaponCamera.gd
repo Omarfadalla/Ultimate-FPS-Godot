4ff38915c75cf44ce906c03b9f2b856b3abcccd6
Omarfadalla <omarfadalla@gmail.com>
2026-07-28 19:26:36 +0300

    fixed some issues in shaders to prevent weapon clipping

---
+++ scripts/WeaponCamera.gd
@@ -0,0 +1,11 @@
+extends Camera3D
+
+@export var MAIN_CAMERA : Node3D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	global_transform = MAIN_CAMERA.global_transform
