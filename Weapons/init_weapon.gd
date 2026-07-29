12ed3279b8becf0aa9aebc768bc6f68e47e2f39e
Omarfadalla <omarfadalla@gmail.com>
2026-07-29 16:50:05 +0300

    added a weapon system

--- Weapons/init_weapon.gd
+++ Weapons/init_weapon.gd
@@ -1,6 +1,11 @@
+@tool
 extends Node3D
 
-@export var WEAPON_TYPE: Weapons
+@export var WEAPON_TYPE: Weapons:
+	set(value):
+		WEAPON_TYPE = value
+		if Engine.is_editor_hint():
+			load_weapon()
 
 @onready var Weapon_mesh : MeshInstance3D = %WeaponMesh
 @onready var Weapon_shadow : MeshInstance3D = %WeaponShadow
@@ -12,5 +17,5 @@ func load_weapon():
 	Weapon_mesh.mesh = WEAPON_TYPE.mesh
 	position = WEAPON_TYPE.position
 	rotation_degrees = WEAPON_TYPE.rotation
-	rotation_degress = WEAPON_TYPE.rotation
+	rotation_degrees = WEAPON_TYPE.rotation
 	Weapon_shadow.visible = WEAPON_TYPE.shadow
